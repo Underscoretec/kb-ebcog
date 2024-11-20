3576d09e1b54322189ac64dc87881b7a1cd39e54
uplTaniya <taniya.kumari@underscoretec.com>
2024-11-20 16:36:06 +0530

    faculty

--- src/components/HomeComponets/FacultySection.tsx
+++ src/components/HomeComponets/FacultySection.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const FacultySection = () => {
   return (
-    <div>FacultySection</div>
+    <div>
+        <h1 className='className="font-oswald text-[55px] font-normal leading-[54px]"'>Diploma leads and Faculty</h1>
+    </div>
   )
 }
 
