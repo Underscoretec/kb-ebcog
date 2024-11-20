dfe76c08a703eacdbb8ce46ad28105f4125740d0
uplTaniya <taniya.kumari@underscoretec.com>
2024-11-20 16:14:52 +0530

    faculty section

--- src/container/home/HomePage.tsx
+++ src/container/home/HomePage.tsx
@@ -1,10 +1,12 @@
 import BannerSection from '@/components/HomeComponets/BannerSection'
+import FacultySection from '@/components/HomeComponets/FacultySection'
 import React from 'react'
 
 const HomePage = () => {
   return (
     <>
       <BannerSection/>
+      <FacultySection />
     </>
   )
 }
