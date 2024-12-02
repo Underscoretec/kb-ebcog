d2f037a4bbda48e0564d099666ef354408c3894a
uplTaniya <taniya.kumari@underscoretec.com>
2024-12-02 19:13:21 +0530

    Build issue solved

--- src/components/FacultyProfile/FacultyPhilosophy.tsx
+++ src/components/FacultyProfile/FacultyPhilosophy.tsx
@@ -1,19 +1,6 @@
 import React from 'react';
 
 const FacultyPhilosophy = ({data}:any) => {
-   
-    // const philosophyData = {
-    //     philosophy: [
-    //         {
-    //             title: "Teaching Philosophy",
-    //             content: "Advancing women's health through innovative research, comprehensive education, and compassionate clinical practice."
-    //         },
-    //         {
-    //             title: "Message to Students",
-    //             content: "Commit to lifelong learning and innovation in obstetrics and gynecology to enhance maternal and fetal health outcomes globally."
-    //         }
-    //     ]
-    // };
 
     return (
         <div className="px-4 md:px-8">
@@ -23,7 +10,7 @@ const FacultyPhilosophy = ({data}:any) => {
                         {item.title}
                     </h2>
                     <div className="text-[#555555] font-montserrat text-[18px] font-normal leading-[27px]">
-                        "{item.content}"
+                        {`"{item.content}"`}
                     </div>
                 </div>
             ))}
