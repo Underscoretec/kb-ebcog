5a1f749a8784eaa697ec417877c96be95c1777c4
uplArpita <arpita.dutta@underscoretec.com>
2024-12-03 16:14:31 +0530

    add scroll funtionality

--- src/common/uicomponents/DotNavigation.tsx
+++ src/common/uicomponents/DotNavigation.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
 
-const DotNavigation = () => {
-  const components = ['Banner', 'About', 'Objective', 'Faculty', 'International', 'FAQ'];
+const DotsNavigation = ({ sections, activeSection }: any) => {
+  const handleScroll = (ref: any) => {
+    ref.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <div className="fixed top-1/2 right-4 transform -translate-y-1/2 flex flex-col gap-4">
-      {components.map((_, index) => (
+      {sections.map((section: any, index: number) => (
         <div
           key={index}
-          className="w-3 h-3 bg-gray-400 rounded-full cursor-pointer hover:bg-yellow-500 transition"
-          title={components[index]}
+          className={`w-2 h-2 rounded-full cursor-pointer transition-all duration-300 ease-in-out ${
+            activeSection === index
+              ? 'w-4 h-4 bg-yellow-500'
+              : 'bg-black hover:w-3 hover:h-3'
+          }`}
+          title={section.title}
+          onClick={() => handleScroll(section.ref)}
         ></div>
       ))}
     </div>
   );
 };
 
-export default DotNavigation;
+export default DotsNavigation;
--- src/container/home/HomePage.tsx
+++ src/container/home/HomePage.tsx
@@ -1,22 +1,50 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import BannerSection from '@/components/HomeComponets/BannerSection';
 import AboutSection from '@/components/HomeComponets/AboutSection';
 import ObjectiveSection from '@/components/HomeComponets/ObjectiveSection';
 import FacultySection from '@/components/HomeComponets/FacultySection';
 import InternationalFaculty from '@/components/HomeComponets/InternationalFaculty';
 import FaqSection from '@/components/HomeComponets/FaqSection';
-import DotsNavigation from '../../common/uicomponents/DotNavigation'; // Import your dots component
+import DotsNavigation from '../../common/uicomponents/DotNavigation';
 
 const HomePage = () => {
+  const bannerRef = useRef(null);
+  const aboutRef = useRef(null);
+  const objectiveRef = useRef(null);
+  const facultyRef = useRef(null);
+  const internationalRef = useRef(null);
+  const faqRef = useRef(null);
+
+  const sections = [
+    { ref: bannerRef, title: 'Banner' },
+    { ref: aboutRef, title: 'About' },
+    { ref: objectiveRef, title: 'Objective' },
+    { ref: facultyRef, title: 'Faculty' },
+    { ref: internationalRef, title: 'International' },
+    { ref: faqRef, title: 'FAQ' },
+  ];
+
   return (
     <>
-      <BannerSection />
-      <AboutSection />
-      <ObjectiveSection />
-      <FacultySection />
-      <InternationalFaculty />
-      <FaqSection />
-      <DotsNavigation /> {/* Add it here */}
+      <div ref={bannerRef}>
+        <BannerSection />
+      </div>
+      <div ref={aboutRef}>
+        <AboutSection />
+      </div>
+      <div ref={objectiveRef}>
+        <ObjectiveSection />
+      </div>
+      <div ref={facultyRef}>
+        <FacultySection />
+      </div>
+      <div ref={internationalRef}>
+        <InternationalFaculty />
+      </div>
+      <div ref={faqRef}>
+        <FaqSection />
+      </div>
+      <DotsNavigation sections={sections} />
     </>
   );
 };
