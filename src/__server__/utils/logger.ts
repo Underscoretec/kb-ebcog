249f53a03bb19b3c86489f59e5ac282c8f3c1a56
uplAyan <ayan.sinha@underscoretec.com>
2024-12-11 12:34:15 +0530

    Build issue fixed

--- src/__server__/utils/logger.ts
+++ src/__server__/utils/logger.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 const winston = require('winston');
 require('winston-daily-rotate-file');
 const { combine, timestamp, printf, colorize, align } = winston.format;
